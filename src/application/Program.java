b343cc55e709507a902a928f51c779ba79433bf6
leandrolattaro <leandrolattaro@gmail.com>
2026-07-17 07:47:29 -0300

    implementation update

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 		Seller newSeller = new Seller (null,"Greg","greg@gmail.com",new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 6: seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 
 	}
 
